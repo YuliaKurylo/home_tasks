1301245cfd103f056d7873f469a8b020b3a7b2f4
Kurylo Yulia <iulia.kurylo@gmail.com>
2017-07-12 17:35:37 +0300

    homework number 8

---
+++ lesson8/js/task2.js
@@ -0,0 +1,5 @@
+var euro = prompt("Put the sum in euro");
+var dollar= prompt("Put the sum in dollar");
+var eurocof=29.58;
+var dollarcof=25.95;
+alert(`${"euros are equal to "}${euro*eurocof}${" UAN, "}${dollar}${" dollars are equal to "}${dollar*dollarcof}${" UAN,"}${" one euro is equal to "}${(eurocof/dollarcof).toFixed(3)}${"dollars"}`);
